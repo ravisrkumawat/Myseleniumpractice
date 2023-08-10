c0728021dddca99377bfe17395b401e4a555e5f8
RK\rvkum <ravi.kumawat@roadcast.net>
2023-08-10 06:56:08 +0530

    Initial commit

---
+++ myfirsttest.py
@@ -0,0 +1,17 @@
+print("Annu")
+a = 10
+print(a)
+# my life is
+
+list = [1, 2, 'Ravi', 'Annu', 1995, 1996]
+list[4] = 'Kumawat'
+print(list[4])
+del list[0:3]
+print(list)
+
+a, b, c, d = 2, 3, 'Great', 1.02
+
+print(a,b,c,d)
+
+print("{}{}{}{}".format('Value of all variables are', a, b, c, d))
+print(type(a))
